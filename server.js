c10571bb2fbdad74faf94d288adf16fc5bbf8e15
iwelnemr <>
2020-08-25 03:30:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ projectData = {};
 
 // Require Express to run server and routes
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 
 // Initialize the main project folder
 app.use(express.static('website'));
